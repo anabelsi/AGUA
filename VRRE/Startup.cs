a95895fa9220a85f351b55c99609037b6f6df786
anabelsi <albraa.nabelsi@outlook.com>
2017-01-23 12:05:44 -0500

    Solution init

---
+++ VRRE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VRRE.Startup))]
+namespace VRRE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
